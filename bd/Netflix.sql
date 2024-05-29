8a9b826b77ddef638855b04d83df54c4bd5c998d
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-29 11:27:34 +0200

    add inserta valores  a la base de datos

--- bd/Netflix.sql
+++ bd/Netflix.sql
@@ -8,3 +8,43 @@ image varchar(1000) not null,
 cathegory varchar(45) not null,
 movieYear int 
 );
+ALTER TABLE movies modify image text;
+ALTER TABLE movies modify image text not null;
+
+CREATE TABLE users (
+idUser int auto_increment primary key,
+user varchar (45) not null,
+password varchar (45) not null,
+name varchar (45) not null,
+email varchar (45) not null,
+plan_details varchar (45) not null
+);
+
+CREATE TABLE actors (
+idActor int auto_increment primary key,
+name varchar (45) not null,
+lastname varchar (45) not null,
+country varchar (45) not null,
+birthday date  
+);
+
+INSERT INTO movies (title, genre, image, cathegory, movieYear) VALUES
+("Pulp Fiction", "Crimen", "https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg", "Top 10", 1994 ),
+("La vita è bella", "Comedia", "https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg", "Top 10", 1996),
+("Forrest Gump", "Comedia", "https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg", "Top 10", 1994 );
+
+INSERT INTO users (user, password,name, email, plan_details) VALUES
+("laura_dev","laura","Laura","laura@gmail.com","Standard"),
+("maria_dev","maria","Maria","maria@gmail.com","Standard"),
+("ester_dev","ester","Ester","ester@gmail.com","Standard");
+
+INSERT INTO actors (name,lastname,country,birthday) VALUES
+("Tom","Hanks","Estados Unidos","1956-07-09"),
+("Roberto","Benigni","Italia","1952-10-27"),
+("John","Travolta","Estados Unidos","1954-02-18");
+
+
+
+
+
+
